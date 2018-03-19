efd2ec82795b1790d9f85e9f434cb260403e70bb
Dmitry Basov <dmbasov@gmail.com>
2018-03-19 22:58:20 +0700

    menu refactoring * reuse same menu components on 'desktop' and 'mobile' layouts * fix mobile menu hamburger

---
+++ src/app/header/menu-mobile/menu-mobile.js
@@ -0,0 +1,18 @@
+import * as utils from "../../core/utils";
+
+const MobileMenu = function (placeHolder, translator, dispatch, { menu }) {
+  
+  this.isMobileMenuOpen = false;
+  placeHolder.on("click", () => switchMobileMenu.call(this));
+
+  d3.select(window).on("resize.mobileMenu", () => switchMobileMenu.call(this, false));
+
+  function switchMobileMenu(force) {
+    this.isMobileMenuOpen = force || force === false ? force : !this.isMobileMenuOpen;
+    menu.classed("open", this.isMobileMenuOpen);
+    placeHolder.classed("open", this.isMobileMenuOpen);
+  }
+  
+}
+
+export default MobileMenu;
