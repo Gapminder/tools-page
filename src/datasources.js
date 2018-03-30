6f9248c27d9550be632aae229210eda72d6e3373
Angie the Fairytale <konzeptmeister@gmail.com>
2018-03-31 00:09:47 +0200

    Switch to develop WS data source

--- src/datasources.js
+++ src/datasources.js
@@ -3,16 +3,16 @@ export default [
   "toolIds": ["bubbles", "mountain", "map", "linechart", "barrank"],
   "datasource": {
     "reader": "waffle", 
-    "assetsPath": "https://import-waffle-server-stage.gapminder.org/api/ddf/assets/",
-    "path": "https://waffle-server-stage.gapminder.org/api/ddf/ql",
+    "assetsPath": "https://import-waffle-server-dev.gapminderdev.org/api/ddf/assets/",
+    "path": "https://waffle-server-dev.gapminderdev.org/api/ddf/ql",
   }
 },
 {
   "toolIds": ["popbyage"],
   "datasource": {
     "reader": "waffle", 
-    "assetsPath": "https://import-waffle-server-stage.gapminder.org/api/ddf/assets/",
-    "path": "https://waffle-server-stage.gapminder.org/api/ddf/ql",
+    "assetsPath": "https://import-waffle-server-dev.gapminderdev.org/api/ddf/assets/",
+    "path": "https://waffle-server-dev.gapminderdev.org/api/ddf/ql",
     "dataset": "open-numbers/ddf--gapminder--population"
   }
 },
