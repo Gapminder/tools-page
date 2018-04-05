6863c38bd817402a9c6abaf56fa99084fc9fe10f
Angie the Fairytale <konzeptmeister@gmail.com>
2018-04-05 19:33:27 +0200

    Update remote build script

---
+++ build.dev.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+eval "$(ssh-agent -s)"
+cd /home/root
+ssh-add /home/root/vizabi_tools_deploy_ssh
+
+# CLONE: Vizabi, Tools Page ------------------------------------
+
+git clone git@github.com:vizabi/vizabi.git -b develop
+git clone git@github.com:vizabi/vizabi-barrankchart.git -b develop
+git clone git@github.com:vizabi/vizabi-bubblechart.git -b develop
+git clone git@github.com:vizabi/vizabi-mountainchart.git -b develop
+git clone git@github.com:vizabi/vizabi-linechart.git -b develop
+git clone git@github.com:vizabi/vizabi-popbyage.git -b develop
+git clone git@github.com:vizabi/vizabi-bubblemap.git -b develop
+
+git clone git@github.com:Gapminder/tools-page.git -b development
+
+# BUILD: Vizabi ------------------------------------------------
+cd vizabi
+echo $PWD && git pull origin develop && rm -rf node_modules && npm i --unsafe-perm && npm run build && cd ..
+cd vizabi-barrankchart
+echo $PWD && git pull origin develop && rm -rf node_modules && npm i --unsafe-perm && npm run build && cd ..
+cd vizabi-bubblechart
+echo $PWD && git pull origin develop && rm -rf node_modules && npm i --unsafe-perm && npm run build && cd ..
+cd vizabi-mountainchart
+echo $PWD && git pull origin develop && rm -rf node_modules && npm i --unsafe-perm && npm run build && cd ..
+cd vizabi-linechart
+echo $PWD && git pull origin develop && rm -rf node_modules && npm i --unsafe-perm && npm run build && cd ..
+cd vizabi-popbyage
+echo $PWD && git pull origin develop && rm -rf node_modules && npm i --unsafe-perm && npm run build && cd ..
+cd vizabi-bubblemap
+echo $PWD && git pull origin develop && rm -rf node_modules && npm i --unsafe-perm && npm run build && cd ..
+
+# BUILD: Tools Page --------------------------------------------
+cd tools-page
+echo $PWD && git pull origin development && rm -rf node_modules && npm i --unsafe-perm
+npm i --unsafe-perm ../vizabi ../vizabi-barrankchart ../vizabi-bubblechart ../vizabi-mountainchart ../vizabi-linechart ../vizabi-popbyage ../vizabi-bubblemap 
+npm rebuild
+
+npm run build:dev
