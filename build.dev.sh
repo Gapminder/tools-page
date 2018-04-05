da7f26a85f646b3f9d74023edc9eaf527b2cc19a
Angie the Fairytale <konzeptmeister@gmail.com>
2018-04-06 00:55:59 +0200

    Add stage script and pretty print

--- build.dev.sh
+++ build.dev.sh
@@ -7,15 +7,16 @@ ssh-add /home/root/vizabi_tools_deploy_ssh
 # BUILD: Tools Page --------------------------------------------
 git clone git@github.com:Gapminder/tools-page.git -b development
 cd tools-page || return
+
 if [ -z "$1" ]; then
-  echo && echo "👉 git pull && rm -rf node_modules && npm i"
-  echo 🗂 $PWD && git pull origin development && rm -rf node_modules && npm i --quiet --depth 0 --unsafe-perm
+  echo && echo 🗂 $PWD && echo "👉 git pull && npm i"
+  git pull origin development && npm i --quiet --depth 0 --unsafe-perm
   echo && echo "👉 npm i all vizabi tools from latest versions"
   npm i --quiet --depth 0 --unsafe-perm vizabi@latest 
   npm i --quiet --depth 0 --unsafe-perm vizabi-barrankchart@latest vizabi-bubblechart@latest vizabi-mountainchart@latest vizabi-linechart@latest vizabi-popbyage@latest vizabi-bubblemap@latest 
 else
-  echo && echo "👉 git pull && npm i"
-  echo 🗂 $PWD && git pull origin development && npm i --quiet --depth 0 --unsafe-perm
+  echo && echo 🗂 $PWD && echo "👉 git pull && npm i"
+  git pull origin development && npm i --quiet --depth 0 --unsafe-perm
   echo && echo "👉 npm i only a specific vizabi part"
   npm i --quiet --depth 0 --unsafe-perm $(echo "$1" | sed -e "s/vizabi\///g")@latest
 fi
---
+++ build.stage.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+eval "$(ssh-agent -s)"
+cd /home/root || return
+ssh-add /home/root/vizabi_tools_deploy_ssh
+
+# BUILD: Tools Page --------------------------------------------
+git clone git@github.com:Gapminder/tools-page.git -b master
+cd tools-page || return
+echo && echo 🗂 $PWD && echo "👉 git pull && rm -rf node_modules && npm i"
+git pull origin master && rm -rf node_modules && npm i --quiet --depth 0 --unsafe-perm
+  
+echo && echo "👉 npm run build:prod"
+npm run build:prod && echo "✅ Tools page build done"
