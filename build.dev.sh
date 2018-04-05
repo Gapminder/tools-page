ebefa3dff09f238aee326f7350933538f09cc3f4
Angie the Fairytale <konzeptmeister@gmail.com>
2018-04-06 01:31:13 +0200

    Attempt to avoid invalid version errors by npm

--- build.dev.sh
+++ build.dev.sh
@@ -9,17 +9,17 @@ git clone git@github.com:Gapminder/tools-page.git -b development
 cd tools-page || return
 
 if [ -z "$1" ]; then
-  echo && echo 🗂 $PWD && echo "👉 git pull && npm i"
-  git pull origin development && npm i --quiet --depth 0 --unsafe-perm
+  echo && echo 🗂 $PWD && echo "👉 git pull && rm -rf node_modules/ && npm i"
+  git pull origin development && rm -rf node_modules/ && npm i --quiet --depth 0 --unsafe-perm
   echo && echo "👉 npm i all vizabi tools from latest versions"
-  npm i --quiet --depth 0 --unsafe-perm vizabi@latest 
-  npm i --quiet --depth 0 --unsafe-perm vizabi-barrankchart@latest vizabi-bubblechart@latest vizabi-mountainchart@latest vizabi-linechart@latest vizabi-popbyage@latest vizabi-bubblemap@latest 
+  npm i --quiet --depth 0 --unsafe-perm -S vizabi@latest vizabi-barrankchart@latest vizabi-bubblechart@latest vizabi-mountainchart@latest vizabi-linechart@latest vizabi-popbyage@latest vizabi-bubblemap@latest 
 else
   echo && echo 🗂 $PWD && echo "👉 git pull && npm i"
   git pull origin development && npm i --quiet --depth 0 --unsafe-perm
   echo && echo "👉 npm i only a specific vizabi part"
-  npm i --quiet --depth 0 --unsafe-perm $(echo "$1" | sed -e "s/vizabi\///g")@latest
+  npm i --quiet --depth 0 --unsafe-perm -S $(echo "$1" | sed -e "s/vizabi\///g")@latest
 fi
   
 echo && echo "👉 npm run build"
 npm run build && echo "✅ Tools page build done"
+git reset --hard origin/development
