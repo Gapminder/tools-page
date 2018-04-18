5590d29bd4b2b29229920ebe43b5d87a1f7e481b
Angie the Fairytale <konzeptmeister@gmail.com>
2018-04-19 01:13:24 +0200

    Fix the prod script

--- build.prod.green.sh
+++ build.prod.green.sh
@@ -2,7 +2,7 @@
 
 cd /home
 
-if [ ! -f ./v$1 ]
+if [ ! -d ./v$1 ]
   then
     echo "🚧 Version v$1 was not found. Doing nothing."
   else
@@ -10,5 +10,5 @@ if [ ! -f ./v$1 ]
     tar cvfz "backup-${now}.tar.gz" live/ && echo "✅ The current version is archived in backup-${now}.tar.gz"
     rm -rf live/ && echo "✅ The current version is deleted"
     mkdir live
-    cp -r v$1/ live/ && echo "✅ Green prod v$1 is now served from live/"
+    cp -r v$1/* live/ && echo "✅ Green prod v$1 is now served from live/"
 fi
