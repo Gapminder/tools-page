3fc42fc990c6547ada3678ff40599c473081be2c
Angie the Fairytale <konzeptmeister@gmail.com>
2018-04-19 10:57:20 +0200

    Imptove prod build scripts

--- build.prod.backup.sh
+++ build.prod.backup.sh
@@ -2,4 +2,9 @@
 
 cd /home
 
-tar cvfz "backup-$1.tar.gz" live/ && echo "✅ The current version is archived in backup-$1.tar.gz"
+if [ -z "$1" ]; then
+  now=$(date +%Y%m%d-%H%M%S)
+  tar cvfz "backup-${now}.tar.gz" live/ && echo "✅ The current version is archived in backup-${now}.tar.gz"  
+else
+  tar cvfz "backup-$1.tar.gz" live/ && echo "✅ The current version is archived in backup-$1.tar.gz"
+fi
--- build.prod.green.sh
+++ build.prod.green.sh
@@ -2,13 +2,13 @@
 
 cd /home
 
-if [ ! -d ./v$1 ]
+if [ ! -d ./$1 ]
   then
-    echo "🚧 Version v$1 was not found. Doing nothing."
+    echo "🚧 Version $1 was not found. Doing nothing."
   else
     now=$(date +%Y%m%d-%H%M%S)
     tar cvfz "backup-${now}.tar.gz" live/ && echo "✅ The current version is archived in backup-${now}.tar.gz"
     rm -rf live/ && echo "✅ The current version is deleted"
     mkdir live
-    cp -r v$1/* live/ && echo "✅ Green prod v$1 is now served from live/"
+    cp -r $1/* live/ && echo "✅ Green prod $1 is now served from live/"
 fi
