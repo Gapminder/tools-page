e6bfa4e124e42faccf2244463fca27e61623ef0f
Jasper <github@jasperheeffer.nl>
2018-05-03 18:31:36 +0200

    Initial commit - static concept list
    Concept list may have to come from configs or dataset later. They are dataset specific and shouldn't be used in other projects.

---
+++ src/app/core/deprecated-url.rules/deprecatedConcepts/conceptMapping.js
@@ -0,0 +1,5 @@
+export default new Map([
+	["co2_intensity_of_economic_output_kg_co2_per_2005_ppp_of_gdp", "co2_intensity_of_economic_output_kg_co2_per_2011_ppp_of_gdp"],
+	["extreme_poverty_percent_people_below_125_a_day", "extreme_poverty_percent_people_below_190_a_day"],
+	["gnipercapita_constant_2000_us", "gnipercapita_constant_2010_us"]
+]);
