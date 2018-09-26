d31e2d6bf9c64d108e60049d4c7c9617387e8202
Angie the Fairytale <konzeptmeister@gmail.com>
2018-09-26 12:12:45 +0200

    Update wording on GW deprecation message and make a softer rule for World referrer detection

--- src/app/core/deprecated-url.rules/worldReferrer.js
+++ src/app/core/deprecated-url.rules/worldReferrer.js
@@ -2,7 +2,7 @@ import message from "./../../header/message/message";
 
 const rule = {
   test: function(url) {
-    return document.referrer.indexOf("//www.gapminder.org/world/") !== -1 || /#\$majorMode/.test(url);
+    return document.referrer.indexOf("gapminder.org/world") !== -1 || /#\$majorMode/.test(url);
   },
 
   use: function(url) {
@@ -11,7 +11,7 @@ const rule = {
 
 	message.showMessage(`<strong>Welcome to Gapminder Tools!</strong><br>
         You came to this page using a link to Gapminder World, our old charts. What you see now is its replacement with newer data and better looks.<br> 
-        We aren't planning to keep the old Gapminder World around for much longer: it's a pain in the neck to maintain and adds infrastructure costs. If you really-really need it, please <a href="mailto:angie@gapminder.org">write us why</a>, let's see how many you are, hopefully not that many.`
+        We aren't planning to keep the old Gapminder World around for much longer: it's a pain in the neck to maintain and adds infrastructure costs. If you really-really need it, please <a href="mailto:angie@gapminder.org">write us why</a>, let's see how many you are, hopefully not that many. The new Gapminder Tools (this page you are looking at) will be developled and maintained further`
     );
     return url;
   }
