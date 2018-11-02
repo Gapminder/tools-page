f8844301604ff303d26a69543498c81dcf56203f
Dmitry Basov <dmbasov@gmail.com>
2018-11-02 23:20:20 +0700

    add properties.json to 'config' for separate values with build mode

--- src/app/core/language.js
+++ src/app/core/language.js
@@ -8,11 +8,12 @@ const DEFAULT_LANGUAGE = { key: 'en', text: 'English' };
 const AVAILABLE_LANGUAGES = [
   DEFAULT_LANGUAGE,
   { key: 'ar-SA', text: 'العربية', isRtl: true },
-//  { key: 'he-IL', text: 'עִבְרִית', isRtl: true },
-//  { key: 'es-ES', text: 'Español', isRtl: false },
-//  { key: 'vi-VN', text: 'Tiếng Việt', isRtl: false, fontFamily: 'Helvetica, Arial, Sans-Serif'},
-//  { key: 'ru-RU', text: 'Русский', isRtl: false, fontFamily: 'Helvetica, Arial, Sans-Serif'}
-];
+  { key: 'he-IL', text: 'עִבְרִית', isRtl: true },
+  { key: 'es-ES', text: 'Español', isRtl: false },
+  { key: 'vi-VN', text: 'Tiếng Việt', isRtl: false, fontFamily: 'Helvetica, Arial, Sans-Serif'},
+  { key: 'ru-RU', text: 'Русский', isRtl: false, fontFamily: 'Helvetica, Arial, Sans-Serif'}
+].filter(({key}) => toolsPage_properties["LANGUAGES"].includes(key));
+
 const TRANSLATE_CONTRIBUTION_LINK = "https://docs.google.com/document/d/1bzTWStFYAq2Oj9kV3vm261Dj9FW8C2tS7Jwj1wtTn1Q";
 
 const TRANSLATION_DICTIONARY = {};
