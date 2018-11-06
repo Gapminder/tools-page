d5c5888e97eae7f3f0498809fa32b0bbe7e3742d
Angie the Fairytale <konzeptmeister@gmail.com>
2018-11-07 00:06:17 +0100

    init Quintiles data lab

--- src/config/toolconfigs/BubbleChart-basic.js
+++ src/config/toolconfigs/BubbleChart-basic.js
@@ -1,7 +1,23 @@
 var VIZABI_MODEL = {
-  "data": {
-    "reader": "csv",
-    "path": "data/csv/basic.csv"
+  state: {
+    marker: {
+      axis_x: {
+        use: "indicator",
+        which: "Household income per capita ($PPP 2011)",
+        scaleType: "log"
+      },
+      color: {
+        which: "GAVI Status",
+        use: "indicator",
+        scaleType: "ordinal"
+      }
+    }
+  },
+  ui: {
+    "datawarning": {
+      "doubtDomain": [1800, 2020],
+      "doubtRange": [0, 0]
+    }
   }
 };
 
