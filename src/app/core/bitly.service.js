189968bcbd947260b297b2f426dccfb1b79ae6ac
Angie the Fairytale <konzeptmeister@gmail.com>
2019-02-19 19:25:05 +0100

    Fix fetching json files for D3 v5

--- src/app/core/bitly.service.js
+++ src/app/core/bitly.service.js
@@ -10,7 +10,7 @@ export default function BitlyService() {
 
       const serviceUrl = `${bitlyUrl}?access_token=${'c5c5bdef4905a307a3a64664b1d06add09c48eb8'}&longUrl=${encodeURIComponent(url)}`;
 
-      return d3.json(serviceUrl, (error, response) => {
+      return Vizabi.utils.d3json(serviceUrl, (error, response) => {
         const bitlyResponse = response;
 
         callback(bitlyResponse.status_code === 200 ? bitlyResponse.data.url : window.location);
