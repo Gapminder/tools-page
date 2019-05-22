67e2e73e6daea9f46373a1de35696415ca272a09
Angie the Fairytale <konzeptmeister@gmail.com>
2019-05-22 12:36:04 +0200

    Migrate to bitly API v4

--- src/app/core/bitly.service.js
+++ src/app/core/bitly.service.js
@@ -1,20 +1,28 @@
 export default function BitlyService() {
 
-  const bitlyUrl = 'https://api-ssl.bitly.com/v3/shorten';
+  const bitlyUrl = 'https://api-ssl.bitly.com/v4/shorten';
 
   return {
     shortenUrl: function(url = document.URL, callback) {
-      // if (!url.includes('gapminder')) {
-      //   return;
-      // }
 
-      const serviceUrl = `${bitlyUrl}?access_token=${'c5c5bdef4905a307a3a64664b1d06add09c48eb8'}&longUrl=${encodeURIComponent(url)}`;
-
-      return Vizabi.utils.d3json(serviceUrl, (error, response) => {
-        const bitlyResponse = response;
-
-        callback(bitlyResponse.status_code === 200 ? bitlyResponse.data.url : window.location);
+      return d3.json(bitlyUrl, {
+        method:"POST",
+        body: JSON.stringify({
+          long_url : url
+        }),
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": "Bearer da63d03dbdcd9d18de75a7a1340dc0aaf3fa3c7f"
+        }
+      })
+      .then(response => {
+        callback(response.link);
+      })
+      .catch(error => {
+        callback(window.location);
       });
     }
   }
 }
+
+
