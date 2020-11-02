a5aee010bdeb7ed44ee08d22add6487878d69763
Angie the Fairytale <konzeptmeister@gmail.com>
2020-11-02 12:40:30 +0100

    Barrankchart debugged till being usable

--- src/config/toolconfigs/BarRankChart-reactive.js
+++ src/config/toolconfigs/BarRankChart-reactive.js
@@ -48,6 +48,7 @@ VIZABI_MODEL = {
           },
           "label": {
             data: {
+              space: ["country"],
               modelType: "entityPropertyDataConfig",
               concept: "name"
             }
