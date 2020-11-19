6a94842e4379cb1cce8ff9cb67b375e57632ccbc
Angie the Fairytale <konzeptmeister@gmail.com>
2020-11-20 00:09:26 +0100

    Enable mountain chart

---
+++ src/config/toolconfigs/MountainChart-reactive.js
@@ -0,0 +1,131 @@
+VIZABI_MODEL = {
+  model: {
+    markers: {
+      "mountain": {
+        data: {
+          locale: "en",
+          source: "sg",
+          space: ["country", "time"],
+        },
+        encoding: {
+          "selected": {
+            modelType: "selection"
+          },
+          "highlighted": {
+            modelType: "selection"
+          },
+          "x": {
+            data: {
+              concept: "income_per_person_gdppercapita_ppp_inflation_adjusted"
+            },
+            scale: {
+              type: "log",
+              domain: [0.11, 500]
+            },
+            "tailFatX": 1.85,
+            "tailCutX": 0.2,
+            "tailFade": 0.7,
+            "xScaleFactor": 1.039781626,
+            "xScaleShift": -1.127066411
+          },
+          "y": {
+            data: {
+              concept: "population_total"
+            }
+          },
+          "s": {
+            data: {
+              concept: "gapminder_gini"
+            }
+          },
+          "color": {
+            data: {
+              space: ["country"],
+              concept: "world_4region"
+            },
+            "scale": {
+              modelType: "color",
+              type: "ordinal"
+            }
+          },
+          "stack": {
+            data: {
+              space: ["country"],
+              constant: "all"
+            }
+          },
+          "group": {
+            data: {
+              space: ["country"],
+              concept: "world_4region"
+            },
+            "merge": false
+          },
+          "label": {
+            data: {
+              space: ["country"],
+              modelType: "entityPropertyDataConfig",
+              concept: "name"
+            }
+          },
+          frame: {
+            modelType: "frame",
+            speed: 200,
+            data: {
+              concept: {
+                autoconfig: {
+                  concept_type: "time"
+                }
+              }
+            }
+          }
+        }
+      },
+      "legend": {
+        data: {
+          ref: {
+            transform: "entityConceptSkipFilter",
+            model: "markers.mountain.encoding.color"
+          }
+        },
+        encoding: {
+          color: {
+            data: {
+              concept: { ref: "markers.mountain.encoding.color.data.concept" },
+              constant: { ref: "markers.mountain.encoding.color.data.constant" }
+            },
+            scale: {
+              modelType: "color",
+              palette: { ref: "markers.mountain.encoding.color.scale.palette" }
+            }
+          },
+          name: { data: { concept: "name" } },
+          rank: { data: { concept: "rank" } },
+          map: { data: { concept: "shape_lores_svg" } }
+        }
+      },
+    }
+  },
+  ui: {
+    //ui
+    "time-slider": {
+      "show_value": false
+    },
+    "buttons": {
+      "buttons": ["colors", "find", "moreoptions", "presentation", "sidebarcollapse", "fullscreen"],
+    },
+    "dialogs": {
+      "dialogs": {
+        "popup": ["colors", "find", "moreoptions"],
+        "sidebar": ["colors", "find"],
+        "moreoptions": ["opacity", "speed", "colors", "presentation", "about"]
+      },
+      "find": {
+        "panelMode": "show",
+        "showTabs": {
+          "country": "open"
+        }
+      }
+    }
+  }
+};
