a0cc8a4faf2381fb745ba2032ea7ba5240c2f8f2
Jasper <github@jasperheeffer.nl>
2021-04-12 19:49:49 +0200

    First version of configless linechart
    (Buggy and not moved inside linechart component yet)

---
+++ src/config/toolconfigs/LineChart-configless.js
@@ -0,0 +1,154 @@
+
+VIZABI_MODEL = {
+  model: {
+    markers: {
+      line: {
+        requiredEncodings: ["x", "y"],
+        data: {
+          filter: {
+            dimensions: {
+              "country": {
+              }
+            }
+          },
+          source: "sg"
+        },
+        encoding: {
+          "selected": {
+            modelType: "selection"
+          },
+          "highlighted": {
+            modelType: "selection"
+          },
+          "y": {
+            scale: {
+              allowedTypes: ["linear", "log", "genericLog", "pow"]
+            }
+          },
+          "x": {
+            data: {
+              concept: { 
+                ref: "markers.line.encoding.frame.data.concept"
+              }
+            },
+            scale: {
+              allowedTypes: ["linear", "log", "genericLog", "pow", "time"]
+            }
+          },
+          "color": {
+            data: {
+              space: { },
+              concept: {
+                filter: { 
+                  concept_type: { $in: ['entity_set', 'entity_domain'] } 
+                }
+              }
+            },
+            scale: {
+              modelType: "color",
+              allowedTypes: ["ordinal", "point"]
+            }
+          },
+          "label": {
+            data: {
+              modelType: "entityPropertyDataConfig",
+            }
+          },
+          frame: {
+            modelType: "frame"
+          }
+        }
+      },
+      "legend": {
+        data: {
+          ref: {
+            transform: "entityConceptSkipFilter",
+            model: "markers.line.encoding.color"
+          }
+        },
+        encoding: {
+          color: {
+            data: {
+              concept: { ref: "markers.line.encoding.color.data.concept" },
+              constant: { ref: "markers.line.encoding.color.data.constant" }
+            },
+            scale: {
+              modelType: "color",
+              palette: { ref: "markers.line.encoding.color.scale.palette" },
+              domain: null,
+              range: null,
+              type: null,
+              zoomed: null,
+              zeroBaseline: false,
+              clamp: false,
+              allowedTypes: null
+            }
+            //scale: { ref: "markers.line.encoding.color.scale" }
+          },
+          name: { data: { concept: "name" } },
+          rank: { data: { concept: "rank" } },
+          map: { data: { concept: "shape_lores_svg" } }
+        }
+      }
+    }
+  },
+  ui: {
+    locale: { id: "en" },
+    layout: { projector: false },
+
+    //ui
+    "time-slider": {
+      "show_value": true
+    },
+    "buttons": {
+      "buttons": ["colors", "find", "moreoptions", "presentation", "sidebarcollapse", "fullscreen"],
+    },
+    "dialogs": {
+      "dialogs": {
+        "popup": ["colors", "find", "moreoptions"],
+        "sidebar": ["colors", "find"],
+        "moreoptions": ["opacity", "speed", "colors", "axes", "presentation", "about"]
+      },
+      "find": {
+        "panelMode": "show",
+        "showTabs": {
+          "country": "open fully"
+        }
+      }
+    },
+    chart: {
+      showForecast: false,
+      showForecastOverlay: true,
+      pauseBeforeForecast: true,
+      opacityHighlight: 1.0,
+      opacitySelect: 1.0,
+      opacityHighlightDim: 0.1,
+      opacitySelectDim: 0.3,
+      opacityRegular: 0.8,
+      hideXAxisValue: false,
+      curve: "curveMonotoneX",
+      whenHovering: {
+        showTooltip: false,
+        hideVerticalNow: false,
+        showProjectionLineX: false,
+        showProjectionLineY: false,
+        higlightValueX: false,
+        higlightValueY: false
+      },
+      labels: {
+        min_number_of_entities_when_values_hide: 3,
+      },
+      datawarning: {
+        doubtDomain: [1800, 1950, 2015],
+        doubtRange: [1.0, 0.3, 0.2]
+      }
+    },
+    "tree-menu": {
+      "folderStrategyByDataset": {
+        "sg": "spread",
+        "fasttrack": "spread",
+        "wdi": "folder:other_datasets"
+      }
+    }
+  }
+};
