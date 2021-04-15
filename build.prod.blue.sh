c0d750d98c265e8fabc31a9dd1a93ffd1ddac98d
Angie <konzeptmeister@gmail.com>
2021-04-15 16:20:31 +0200

    Update tools page bash script

--- build.prod.blue.sh
+++ build.prod.blue.sh
@@ -27,5 +27,18 @@ npm run build:prod && echo "✅ Tools page build done. Blue prod is served from 
 
 echo && echo 🗂 $PWD && echo "👉 copy build output into a special folder for this version"
 VERSION="$(json -f package.json dependencies.vizabi-reactive)"
+COMMIT_ID="$(git rev-parse --verify HEAD)"
 cd /home 
-rm -rf "v${VERSION}" && mkdir "v${VERSION}" && cp -r tools-page/build/tools/* "v${VERSION}" && echo "✅ Build output copied to folder v${VERSION}. Served at http://tools-blue.gapminder.org:8080/tools/"
+rm -rf "v${VERSION}" && mkdir "v${VERSION}" && cp -r tools-page/build/tools/* "v${VERSION}" \
+&& echo -e "✅ Build output is served from /home/tools-page/build at http://tools-blue.gapminder.org:8080/tools/ \n"\
+"and copied to folder v${VERSION}. Run sh green.sh v${VERSION} to make it live."
+
+echo && echo 🗂 $PWD && echo "👉 Rollbar source maps management"
+ROLLBAR_TOKEN="eb246f4a36a34433b86b8490187aa972"
+TOOLSPAGE_HOST="https://gapminder.org/tools/"
+cd /home/"v${VERSION}" && sed -i -e "s/version-to-be-replaced-by-a-bash-script/${COMMIT_ID}/g" ./index.html \
+&& echo "✅ replaced version in v${VERSION}/index.html"
+curl https://api.rollbar.com/api/1/sourcemap -F access_token="${ROLLBAR_TOKEN}" -F version="${COMMIT_ID}" -F minified_url="${TOOLSPAGE_HOST}"toolspage.min.js -F source_map=@toolspage.min.js.map \
+&& curl https://api.rollbar.com/api/1/sourcemap -F access_token="${ROLLBAR_TOKEN}" -F version="${COMMIT_ID}" -F minified_url="${TOOLSPAGE_HOST}"tools.min.js -F source_map=@tools.min.js.map \
+&& curl https://api.rollbar.com/api/1/sourcemap -F access_token="${ROLLBAR_TOKEN}" -F version="${COMMIT_ID}" -F minified_url="${TOOLSPAGE_HOST}"vendor.min.js -F source_map=@vendor.min.js.map \
+&& echo "✅ scheduled uploading of source maps to rollbar API"
