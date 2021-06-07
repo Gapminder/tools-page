8bff02e8c8383bdbaa1c8a71a06a5974e52c5b85
Dmitry Basov <dmbasov@gmail.com>
2021-06-07 14:50:17 +0300

    restore 'spreadsheet' tool

--- src/app/core/tool.js
+++ src/app/core/tool.js
@@ -39,7 +39,7 @@ function removeTool() {
 
 function googleAnalyticsLoadEvents(viz) {
   const markers = viz.model.markers;
-  const markerId = ["bubble", "line", "bar", "mountain", "pyramid"].find(id => markers[id]);
+  const markerId = ["bubble", "line", "bar", "mountain", "pyramid", "spreadsheet"].find(id => markers[id]);
   const marker = markers[markerId];
   const splashMarker = viz.splashMarker;
 
---
+++ src/config/toolconfigs/Spreadsheet-reactive.js
@@ -0,0 +1,21 @@
+VIZABI_MODEL = {
+  model: {
+    markers: {
+      "spreadsheet": {
+        data: {
+          source: "sg"
+        }
+      }
+    }
+  },
+  ui: {
+    chart: {},
+    "tree-menu": {
+      "folderStrategyByDataset": {
+        "sg": "spread",
+        "fasttrack": "spread",
+        "wdi": "folder:other_datasets"
+      }
+    }
+  }
+}
