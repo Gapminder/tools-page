205b63b8e1feaf747382b61f01eebf1d67c44cd9
angie <konzeptmeister@gmail.com>
2022-01-11 11:17:10 +0100

    Dump E2E and all deps

--- e2e/helpers/waitHelper.ts
+++
@@ -1,8 +0,0 @@
-import { browser, ElementFinder, ExpectedConditions as EC } from 'protractor';
-import { ExtendedElementFinder } from './ExtendedElementFinder';
-
-const TIMEOUT = 30000;
-
-export async function waitUntil(element: ElementFinder | ExtendedElementFinder) {
-  return await browser.wait(EC.visibilityOf(element), TIMEOUT, `element ${element.locator().value} not visible`);
-}
--- e2e/pageObjects/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { Footer } from './footer.e2e-component';
-export { Header } from './header.e2e-component';
-export { Slider } from './slider.e2e-component';
-export { Sidebar } from './sidebar.e2e-component';
