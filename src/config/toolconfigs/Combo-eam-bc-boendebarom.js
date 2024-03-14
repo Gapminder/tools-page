800aa2121e28d443c467fd4a6459a19b9fbbdf98
angie <konzeptmeister@gmail.com>
2024-03-14 18:27:12 +0100

    Init boendebarometer config for DDF dataset

---
+++ src/config/toolconfigs/Combo-eam-bc-boendebarom.js
@@ -0,0 +1,312 @@
+export const VIZABI_MODEL = {
+  model: {
+    markers: {
+      bubble: {
+        requiredEncodings: ["x", "y", "centroid", "size"],
+        data: {
+          locale: "en",
+          source: "boendebarom",
+          space: ["deso", "year"],
+        },
+        encoding: {
+          "selected": {
+            modelType: "selection",
+            data: {
+              filter: {
+                ref: "markers.bubble.encoding.trail.data.filter"
+              }
+            }
+          },
+          "highlighted": {
+            modelType: "selection"
+          },
+          "superhighlighted": {
+            modelType: "selection"
+          },
+          "order": {
+            modelType: "order",
+            direction: "desc",
+            data: {
+              ref: "markers.bubble.config.encoding.size.data"
+            }
+          },
+          "size": {
+            data: {
+              concept: "m_syssel_deso_20_64"
+            },
+            scale: {
+              extent: [0, 0.05],
+              modelType: "size",
+              allowedTypes: ["linear", "log", "genericLog", "pow"]
+            }
+          },
+          "y": {
+            data: {
+              concept: "m_efterg_deso_25_64",
+            },
+            scale: {
+              allowedTypes: ["linear", "log", "genericLog", "pow", "time"]
+            }
+          },
+          "x": {
+            data: {
+              concept: "m_dispin_deso_20_64"
+            },
+            scale: {
+              type: "log",
+              zoomed: [100, 1000],
+              allowedTypes: ["linear", "log", "genericLog", "pow", "time"]
+            }
+          },
+          "color": {
+            data: {
+              concept: "kommun"
+            },
+            scale: {
+              modelType: "color",
+              type: "ordinal"
+            }
+          },
+          "label": {
+            data: {
+              modelType: "entityPropertyDataConfig",
+              concept: "name"
+            }
+          },
+          "size_label": {
+            data: {
+              constant: "_default"
+            },
+            scale: {
+              modelType: "size",
+              allowedTypes: ["linear", "log", "genericLog", "pow", "point", "ordinal"]
+            }
+          },
+          frame: {
+            modelType: "frame",
+            speed: 200,
+            value: "2017",
+            splash: true,
+            data: {
+              concept: "year"
+            }
+          },
+          "trail": {
+            modelType: "trail",
+            groupDim: "year",
+            show: true
+          },
+          "centroid": {
+            data: {
+              space: ["deso", "year"],
+              concept: "deso"
+            }
+          },
+          // "lat": {
+          //   data: {
+          //     space: ["deso"],
+          //     concept: "latitude"
+          //   }
+          // },
+          // "lon": {
+          //   data: {
+          //     space: ["deso"],
+          //     concept: "longitude"
+          //   }
+          // },
+          "color_map": {
+            data: {
+              concept: "m_syssel_deso_20_64"
+            },
+            scale: {
+              modelType: "color"
+            }
+          },
+        }
+      },
+      "legend": {
+        data: {
+          ref: {
+            transform: "entityConceptSkipFilter",
+            path: "markers.bubble.encoding.color"
+          }
+        },
+        encoding: {
+          color: {
+            data: {
+              concept: { ref: "markers.bubble.encoding.color.data.concept" },
+              constant: { ref: "markers.bubble.encoding.color.data.constant" }
+            },
+            scale: {
+              modelType: "color",
+              palette: { ref: "markers.bubble.encoding.color.scale.palette" },
+              domain: null,
+              range: null,
+              type: null,
+              zoomed: null,
+              zeroBaseline: false,
+              clamp: false,
+              allowedTypes: null
+            }
+            //scale: { ref: "markers.bubble.encoding.color.scale" }
+          },
+          name: { data: { concept: "name" } },
+          order: {
+            modelType: "order",
+            direction: "asc",
+            data: { concept: "rank" }
+          },
+          map: { data: { concept: "shape_lores_svg" } }
+        }
+      },
+      "legend_map": {
+        data: {
+          ref: {
+            transform: "entityConceptSkipFilter",
+            path: "markers.bubble.encoding.color_map"
+          }
+        },
+        encoding: {
+          color: {
+            data: {
+              concept: { ref: "markers.bubble.encoding.color_map.data.concept" },
+              constant: { ref: "markers.bubble.encoding.color_map.data.constant" }
+            },
+            scale: {
+              modelType: "color",
+              palette: { ref: "markers.bubble.encoding.color_map.scale.palette" }
+            }
+            //scale: { ref: "markers.bubble.encoding.color.scale" }
+          },
+          name: { data: { concept: "name" } },
+          rank: { data: { concept: "rank" } },
+          map: { data: { concept: "shape_lores_svg" } }
+        }
+      }
+    }
+  },
+  ui: {
+    locale: { id: "en" },
+    layout: { projector: false },
+
+    //ui
+    "buttons": {
+      "buttons": ["colors", "mapcolors", "find", "trails", "moreoptions", "presentation", "sidebarcollapse", "fullscreen"]
+    },
+    "dialogs": {
+      "dialogs": {
+        "popup": ["colors", "find", "moreoptions"],
+        "sidebar": ["colors", "find", "mapcolors", "zoom"],
+        "moreoptions": [
+          "opacity",
+          "speed",
+          "axes",
+          "size",
+          "colors",
+          "mapcolors",
+          "mapoptions",
+          "label",
+          "zoom",
+          "technical",
+          "repeat",
+          "presentation",
+          "about"
+        ]
+      },
+      "find": {
+        enableSelectShowSwitch: false,
+        enableMarkerSpaceOptions: false,
+      }
+    },
+
+    "chart": {
+      show_ticks: true,
+      showForecast: false,
+      showForecastOverlay: true,
+      pauseBeforeForecast: true,
+      endBeforeForecast: "2022",
+      opacityHighlight: 1.0,
+      opacitySelect: 1.0,
+      opacityHighlightDim: 0.1,
+      opacitySelectDim: 0.3,
+      opacityRegular: 0.8,
+      yearInBackground: true,
+      yearInTrails: true,
+      lockNonSelected: 0,
+      numberFormatSIPrefix: true,
+      panWithArrow: true,
+      adaptMinMaxZoom: false,
+      cursorMode: "arrow",
+      zoomOnScrolling: true,
+      superhighlightOnMinimapHover: true,
+      splitVertical: true,
+      splitRatio: 0.4,
+      whenHovering: {
+        showProjectionLineX: true,
+        showProjectionLineY: true,
+        higlightValueX: true,
+        higlightValueY: true
+      },
+      labels: {
+        enabled: true,
+        dragging: true,
+        removeLabelBox: false
+      },
+      margin: {
+        left: 0,
+        top: 0
+      },
+      decorations: {
+        "enabled": true,
+        "xAxisGroups": {
+          "income_per_person_gdppercapita_ppp_inflation_adjusted": [
+            { "min": null, "max": 2650, "label": "incomegroups/level1", "label_short": "incomegroups/level1short" },
+            { "min": 2650, "max": 8000, "label": "incomegroups/level2", "label_short": "incomegroups/level2short" },
+            { "min": 8000, "max": 24200, "label": "incomegroups/level3", "label_short": "incomegroups/level3short" },
+            { "min": 24200, "max": null, "label": "incomegroups/level4", "label_short": "incomegroups/level4short" }
+          ]
+        }
+      },
+      "map": {
+        "scale": 1,
+        "preserveAspectRatio": true,
+        "mapEngine": "mapbox",
+        "mapStyle": "mapbox://styles/mapbox/light-v9",
+        "showBubbles": false,
+        "showAreas": true,
+        "showMap": true,
+        "offset": {
+          "top": 0.05,
+          "bottom": -0.12,
+          "left": 0,
+          "right": 0
+        },
+        "path": null,
+        "bounds": {
+          west: 17.6, north: 59.5, east: 18.2, south: 59.2
+        },
+        "projection": "mercator",
+        topology: {
+          path: "assets/01_03_wgs84.json",
+          objects: {
+            geo: "01_03_wgs84_geojson",
+            boundaries: "01_03_wgs84_geojson"
+          },
+          geoIdProperty: "Deso",
+        }
+      }
+    },
+    "data-warning": {
+      doubtDomain: [1800, 1950, 2015],
+      doubtRange: [0, 0, 0]
+    },
+    "tree-menu": {
+      "showDataSources": false,
+      "folderStrategyByDataset": {
+        "sg": "spread",
+        "fasttrack": "spread",
+        "wdi": "folder:other_datasets"
+      }
+    }
+  }
+};
