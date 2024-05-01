a57cf9ce4dc2736b131d598c4da6ad1b608db7ad
angie <konzeptmeister@gmail.com>
2024-05-01 18:29:30 +0200

    Add URL modernisation logic from V1 to V2

--- src/config/entitysetMapping.js
+++ src/config/entitysetMapping.js
@@ -1,6 +1,17 @@
+/*
+if the URL points to one of the listed "chartTypes"
+and contains keys among "entitySets": {key: value, anotherkey: anothervalue} in expressions like
+"$" + key + "$/$in"
+"$" + key + "="
+
+these keys will be replaced with their corresponding values
+"$" + value + "$/$in"
+"$" + value + "="
+*/
+
 const toolsPage_entitysetMapping = {
   "chartTypes": ["bubbles", "map", "linechart", "barrank", "spreadsheet"],
   "entitySets": {
-    "geo": "country"
+    //"country": "geo"  this moved to upgradeUrlV1toV2.js
   }
 };
