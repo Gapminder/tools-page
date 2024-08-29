041958fef99f57f515c34da64054fe96dea6cdbe
angie <konzeptmeister@gmail.com>
2024-08-29 18:50:06 +0200

    Add extapi map and spreadsheet tools

---
+++ src/config/toolconfigs/Spreadsheet-boendebarom.js
@@ -0,0 +1,24 @@
+export const VIZABI_MODEL = {
+  model: {
+    markers: {
+      "spreadsheet": {
+        data: {
+          source: "boendebarom"
+        }
+      }
+    }
+  },
+  ui: {
+    chart: {},
+    "tree-menu": {
+      "showDataSources": false,
+      "folderStrategyByDataset": {
+        "kolada": "spread",
+        "boendebarom": "spread",
+        "wdi": "folder:other_datasets"
+      }
+    }
+  }
+};
+
+
