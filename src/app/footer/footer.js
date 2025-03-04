86b17baf8fef8d2b3aa1043c3e9c90372127eea6
angie <konzeptmeister@gmail.com>
2025-03-04 23:35:34 +0100

    Styling updates useful for tools page

--- src/app/footer/footer.js
+++ src/app/footer/footer.js
@@ -3,21 +3,19 @@ import * as utils from "../core/utils";
 const Footer = function(placeHolder, translator, dispatch) {
   const templateHtml = `
     <div class="footer-container">
-        <div class="footer-container menu-holder">
-            <div class="logo-gray">
-                <img src="assets/images/logo-gray.png">
-            </div>
-            <div class="general-menu">
-                <ul class="nav">
-                    <li><a href="https://visual-encodings.com/" data-text="madeby"></a></li>
-                </ul>
-            </div>
-            
+      <div class="footer-container menu-holder">
+
+        <div class="general-menu">
+          <ul class="nav">
+            <li><a href="https://visual-encodings.com/" data-text="madeby"></a></li>
+          </ul>
         </div>
-        <div class="footer-container service-container">
-            <div class="service-content">
-            </div>
-        </div>
+          
+      </div>
+      <div class="logos-holder">
+        <img src="assets/images/gapminder_word_logo.svg" height="30px">
+      </div>
+
     </div>
   `;
   //require("./footer.html");
