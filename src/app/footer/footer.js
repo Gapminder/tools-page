1c4984a0c899feadcb261d16ad348e1b1dc43759
angie <konzeptmeister@gmail.com>
2025-03-04 23:45:22 +0100

    Footer update for UU

--- src/app/footer/footer.js
+++ src/app/footer/footer.js
@@ -4,15 +4,26 @@ const Footer = function(placeHolder, translator, dispatch) {
   const templateHtml = `
     <div class="footer-container">
       <div class="footer-container menu-holder">
-
+        <div class="bottom-header">BOENDEBAROMETERN</div>
+        <div class="bottom-text">«The world cannot be understood without numbers, and it cannot be understood with numbers alone. Love numbers for what they tell you about real lives.» — Hans Rosling</div>
         <div class="general-menu">
           <ul class="nav">
-            <li><a href="https://visual-encodings.com/" data-text="madeby"></a></li>
+            <li>
+              <span data-text="madeby"></span>
+              <a href="https://visual-encodings.com/">Visual Encodings AB</a>
+              <span data-text="incollabwith"></span>
+              <a href="https://www.gapminder.org/">Gapminder</a>
+              <span data-text="supportedby"></span>
+              <a href="https://www.lansforsakringar.se/uppsala/foretag/om-oss/">Länsförsäkringar</a>
+              <span data-text="assignmentby"></span>
+              <a href="https://www.uu.se/institution/bostads-och-urbanforskning">Institutet för bostads- och urbanforskning (IBF)</a>
+            </li>
           </ul>
         </div>
           
       </div>
       <div class="logos-holder">
+        <img src="assets/images/lf_logo.svg" height="100px">
         <img src="assets/images/gapminder_word_logo.svg" height="30px">
       </div>
 
@@ -33,7 +44,7 @@ const Footer = function(placeHolder, translator, dispatch) {
   });
 
   function translate() {
-    placeHolder.selectAll("ul.nav li a")
+    placeHolder.selectAll("ul.nav li span")
       .each(utils.translateNode(translator));
   }
 
