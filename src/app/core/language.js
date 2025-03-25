baa80dab208697b817cfab75a48cb25cbef0dfaf
angie <konzeptmeister@gmail.com>
2025-03-25 09:14:12 +0100

    Language: make it show key string if no translation found

--- src/app/core/language.js
+++ src/app/core/language.js
@@ -22,11 +22,12 @@ function setLocalePageClasses() {
 }
 
 function translator(key) {
-  if (availableLocales.includes(key)) return getLocaleDisplayName(key);
+  //get name of language for language switchers and such
+  if(availableLocales.includes(key)) return getLocaleDisplayName(key);
   const locale = getStateLocale();
-  return dictionary[locale] && dictionary[locale][key]
-    || dictionary[defaultLocale] && dictionary[defaultLocale][key]
-    || null;
+  return dictionary[locale] && dictionary[locale][key] 
+    || dictionary[defaultLocale] && dictionary[defaultLocale][key] 
+    || key;
 }
 
 function loadData(locale, prefix = "page", folder = "i18n") {
