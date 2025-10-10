154c82362d723e45cfa4cde94ec922be410e3a11
dab2000 <dmbasov@gmail.com>
2025-10-10 22:56:19 +0300

    add permalinks 'share a link' form for permalink creation support toolspage load for permalink http://..../?for=[slug]

--- src/app/auth/supabase.service.js
+++ src/app/auth/supabase.service.js
@@ -1,6 +1,37 @@
 import toolsPage_properties from "toolsPage_properties";
 const supabaseClient = supabase.createClient(toolsPage_properties.S_URL, toolsPage_properties.S_KEY);
 
+async function userSignup(email, password) {
+  const { data, error } = await supabaseClient.auth.signUp({ email, password });
+  console.log(error, data);
+  return error ? false : true;
+}
+
+async function userLogin(email, password) {
+  const { data, error } = await supabaseClient.auth.signInWithPassword({ email, password });
+  console.log(error, data);
+  return error ? false : true;
+}
+
+async function userLogout() {
+  const { error } = await supabaseClient.auth.signOut();
+  return error ? false : true;
+}
+
+async function isLogged() {
+  const { data, error } = await supabaseClient.auth.getSession();
+  return { 
+    isLogged: data.session && true,
+    session: data.session
+  };
+}
+  
+
+
 export {
-  supabaseClient
+  supabaseClient,
+  userLogin,
+  userLogout,
+  userSignup,
+  isLogged
 };
--- src/app/core/bitly.service.js
+++ src/app/core/bitly.service.js
@@ -1,29 +1,54 @@
-export default function BitlyService() {
+import { isLogged } from "../auth/supabase.service";
+import { createShareLinkModal } from "../header/social-buttons/share-link";
+import { checkSlugAvailability, saveSlug } from "./links-resolve";
+import { randomSlug } from "./utils";
+
+export default async function BitlyService({ state }) {
 
   const bitlyUrl = "https://api-ssl.bitly.com/v4/shorten";
 
   return {
-    shortenUrl(url = document.URL, callback) {
+    async shortenUrl(url = document.URL, callback) {
 
-      return d3.json(bitlyUrl, {
-        method: "POST",
-        body: JSON.stringify({
-          long_url: url
-        }),
-        headers: {
-          "Content-type": "application/json",
-          "Authorization": "Bearer da63d03dbdcd9d18de75a7a1340dc0aaf3fa3c7f"
+      return await isLogged().then(async logged => {
+        if (logged.isLogged) {
+          return createShareLinkModal({
+            slug: randomSlug(state.getTool() + "-"),
+            baseUrl: location.origin + location.pathname + "?for=",
+            checkSlugAvailability,
+            onSave: async({ url, slug, lifetime }) => {
+              saveSlug({ 
+                onSave: ({url}) => callback(url), 
+                url, 
+                userId: logged.session.user.id, 
+                slug, 
+                lifetime, 
+                pageConfig: state.getURLI() 
+              });
+            }
+          });
+        } else {
+          return d3.json(bitlyUrl, {
+            method: "POST",
+            body: JSON.stringify({
+              long_url: url
+            }),
+            headers: {
+              "Content-type": "application/json",
+              "Authorization": "Bearer da63d03dbdcd9d18de75a7a1340dc0aaf3fa3c7f"
+            }
+          })
+            .then(response => {
+              callback(response.link);
+            })
+            .catch(error => {
+              console.error(error);
+              callback(window.location);
+            });
         }
-      })
-        .then(response => {
-          callback(response.link);
-        })
-        .catch(error => {
-          console.error(error);
-          callback(window.location);
-        });
+      });
     }
-  };
+  }
 }
 
 
