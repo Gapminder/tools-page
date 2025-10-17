c7b7e9d7405cfa2e4cc18300b0efba6f7fb7d90e
angie <konzeptmeister@gmail.com>
2025-10-17 17:52:14 +0200

    MODERNISE G TOOLS PAGE: spreadsheet

--- src/config/toolconfigs/Spreadsheet-reactive.js
+++ src/config/toolconfigs/Spreadsheet-reactive.js
@@ -3,13 +3,73 @@ export const VIZABI_MODEL = {
     markers: {
       "spreadsheet": {
         data: {
-          source: "sg"
+          source: "sg",
+          space: ["geo", "time"],
+          filter: {
+            dimensions: { "geo": { "$or": [{ "un_state": true }] } }
+          }
+        },
+        encoding: {
+          "show": {
+            modelType: "selection",
+            data: {
+              filter: { dimensions: { "geo": { "$not": { "is--country": 1, "un_state": 0 } } } }
+            }
+          },
+          "selected": {
+            modelType: "selection"
+          },
+          "highlighted": {
+            modelType: "selection"
+          },
+          "superhighlighted": {
+            modelType: "selection"
+          },
+          number: {
+            data: {
+              concept: "lex"
+            }
+          },
+          label: {
+            data: {
+              modelType: "entityPropertyDataConfig",
+              concept: "name"
+            }
+          },
+          "color": {
+            data: {
+              constant: "_default"
+            },
+            scale: {
+              modelType: "color",
+              type: "ordinal"
+            }
+          },
         }
       }
     }
   },
   ui: {
-    chart: {},
+    "buttons": {
+      "buttons": []
+    },
+    "dialogs": {
+      "dialogs": {
+        "popup": [],
+        "sidebar": [],
+        "moreoptions": []
+      },
+      "markercontrols": {
+        "disableSlice": true,
+        "disableAddRemoveGroups": false,
+        "primaryDim": "geo",
+        "drilldown": "country",
+        "shortcutForSwitch": false
+      },
+    },
+    chart: {
+      opacitySelectDim: 0.3,
+    },
     "tree-menu": {
       "showDataSources": false,
       "folderStrategyByDataset": {
@@ -20,3 +80,4 @@ export const VIZABI_MODEL = {
     }
   }
 };
+
